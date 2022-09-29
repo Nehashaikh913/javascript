6565f9d856c44752651fdc4f44e844b45cc54cc4
nehashaikh913 <nehashaikh0500@gmail.com>
2022-09-30 01:55:01 +0530

    c

--- Menu/menu.js
+++ Menu/menu.js
@@ -71,34 +71,50 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "quarantine buddy",
+    category: "Dinner",
+    price: 16.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
 // get parent element
 const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
-const filterBtns = document.querySelectorAll(".filter-btn");
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
   diplayMenuItems(menu);
-  const categories = menu.map(function(item){
-    return item.categories
+  const categories = menu.reduce(function(values,item){
+  if(!values.includes(item.category)){
+    values.push(item.category)
+  }
+    return values;
+  },["all"]);
+  
+  const categoryBtns=categories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
   })
-});
-
-filterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    // console.log(e.currentTarget.dataset);
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      // console.log(menuItem.category);
-      if (menuItem.category === category) {
-        return menuItem;
+  .join("");
+  btnContainer.innerHTML=categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
       }
     });
-    if (category === "all") {
-      diplayMenuItems(menu);
-    } else {
-      diplayMenuItems(menuCategory);
-    }
   });
 });
 
--- Question/question.js
+++ Question/question.js
@@ -2,24 +2,40 @@
 // allbtn.forEach(function(btn){
 //     btn.addEventListener("click",function(e){
 //        const hello=(e.currentTarget.parentElement.parentElement);
-//        hello.classList.toggle("show-text")
+//        hello.classList.toggle("show-text");
 //     })
 // })
 
 
 
-const questions=document.querySelectorAll(".question")
-questions.forEach(function(hello){
-    const btn=hello.querySelector(".question-btn")
-    btn.addEventListener("click",function(){
+// const questions=document.querySelectorAll(".question")
+// questions.forEach(function(hello){
+//     const btn=hello.querySelector(".question-btn")
+//     btn.addEventListener("click",function(){
 
-        questions.forEach(function(item){
-            if(item!==hello){
-                item.classList.remove("show-text")
+//         questions.forEach(function(item){
+//             if(item!==hello){
+//                 item.classList.remove("show-text")
+//             }
+//         })
+
+
+//         hello.classList.toggle("show-text")
+//     })
+// })
+
+
+const questions=document.querySelectorAll(".question")
+
+questions.forEach(hello => {
+    const btn=hello.querySelector(".question-btn")
+    $(btn).click(function(){
+        questions.forEach(item=>{
+            if(item !==hello){
+                $(item).removeClass("show-text");
             }
         })
-
-
-        hello.classList.toggle("show-text")
-    })
-})
+        $(hello).toggleClass("show-text");
+      });
+      
+});
